eb3657de4c965b6557ba9fcdd052e82738256330
polit8647 <polit8647@gmail.com>
2025-10-15 14:46:25 +0900

    init_commit

--- Assets/03.Prefabs/Prefab.cs
+++ Assets/03.Prefabs/Prefab.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class branchTest : MonoBehaviour
+public class Prefab : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Git_Connect_test"); 
+        
     }
 
     // Update is called once per frame
